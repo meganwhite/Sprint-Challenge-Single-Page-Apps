8d2f47a5a65e3dd8bb283e5042aa344eb449fda5
Megan White <meganwhite@Megans-MacBook-Pro.local>
2019-07-26 11:42:22 -0500

    render location list

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,7 +1,19 @@
 import React from 'react'
 
-export default function LocationCard ({ name, type, dimension, residents }) {
-  // image={image}
-  return (<span>todo: location</span>
+export default function LocationCard (props) {
+  const { name, type, dimension, residents } = props;
+  return (
+    <div className = "location-card">
+      <h2>{name}</h2>
+      <p>{type}</p>
+      <p>{dimension}</p>
+      <p>Residents: </p>
+      {residents.map(resident => (
+        <div key = {resident} className = "resident">
+          {resident}
+        </div>
+      ))}
+    </div>
   )
 }
+
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,5 +1,50 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from "react-router-dom";
+import LocationCard from './LocationCard'
 
 export default function LocationsList() {
+    const[locationsList, setLocationsList] = useState([]);
+
+
+
+    useEffect(() => {
+        // TODO: Add AJAX/API Request here - must run in `useEffect`
+        //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+        const getLocations = () => {
+          axios
+            .get('https://rickandmortyapi.com/api/location/')
+            .then(response => {
+              setLocationsList(response.data.results)
+              
+            })
+            .catch(error => {
+              console.error('Server Error', error);
+            });
+          }
+    
+          getLocations();
+    
+      }, [])
+
+
+      return (
+        <section className='locations-list grid-view'>
+          {locationsList.map(location => {
+            return (
+                <Link to = {`/locations/${location.id}`}>
+                  <LocationCard 
+                  location = {location}
+                  key = {location.id}
+                  name = {location.name}
+                  type = {location.type}
+                  dimension = {location.dimension}
+                  residents = {location.residents}
+                  />
+                </Link>
+            )
+          })}
+        </section>
+      )
 
 }
