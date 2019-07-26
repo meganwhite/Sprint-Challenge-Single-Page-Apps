3a6c47a826dec4bee6e324807a0d78fce2e0d316
Megan White <meganwhite@Megans-MacBook-Pro.local>
2019-07-26 13:02:25 -0500

    added styled components

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,17 +1,20 @@
 import React from 'react'
+import {Card, CardText, Image} from "./StyledWidgets";
 
 export default function CharacterCard(props) {
   const {image,name,species,status,location, origin} = props;
   return (
-    <div className = "character-card">
-      <img src={image} alt={name}/>
-      <h2>{name}</h2>
-      <div className="species-status">
-        <p>{species} {status}</p>
-      </div>
-      <p>Location: {location.name}</p>
-      <p>Origin: {origin.name}</p>
-    </div>
+    <Card className = "character-card">
+      <Image src={image} alt={name}/>
+      <CardText>
+        <h2>{name}</h2>
+        <div className="species-status">
+          <p>{species} {status}</p>
+        </div>
+        <p>Location: {location.name}</p>
+        <p>Origin: {origin.name}</p>
+      </CardText>
+    </Card>
   )
 }
 
---
+++ src/components/StyledWidgets.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+  border: solid 1px #b3b3b3;
+  border-radius: 5px;
+  margin: 1rem;
+  width: 350px;
+`;
+
+export const CardText = styled.div`
+    text-decoration: none;
+    color: black;
+    display: flex;
+    flex-direction: column;
+    padding: 1rem;
+`;
+
+export const Image = styled.img`
+    width: 100%;
+`
